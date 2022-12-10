3f25708980687f5a8b134fc7fc54d81d41d23c46
Muhammed <Muhammed>
2022-12-10 23:49:34 +0100

    first commit

---
+++ src/components/HeroPage.js
@@ -0,0 +1,65 @@
+import classes from "./HeroPage.module.css";
+import { useRef } from "react";
+import { useHistory } from "react";
+
+
+function HeroPageSection(props) {
+        const imageInputRef = useRef();
+    
+        function submitHandler(event) {
+            event.preventDefault();
+    
+            const enteredImage = imageInputRef.current.value;
+    
+            const movieData = {
+                image:enteredImage
+            };
+    
+           props.submitHandler(movieData);
+        }
+
+    /*************Adding API********** */
+    const history = useHistory();
+    function onSearchMovie(movieData) {
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        headers.append("Accept", "application/json");
+        headers.append("Origin", "htttp://localhost:3000");
+        fetch(
+            'http://img.omdbapi.com/?apikey=[yourkey]&',
+          {
+            method: 'POST',
+            body: JSON.stringify(movieData),
+            headers: Headers
+          }
+        ).then(() => {
+            history.replace()
+        })
+
+    };
+
+    
+    return (
+      <section>
+        <div className={classes.top}>
+            <div className={classes.border}>
+            <button className={classes.texts}>MyTestApp</button>
+            </div>
+        </div>   
+        <div className={classes.background}>
+            <div className={classes.title}>
+             <h1 className={classes.text}>Watch Something Incredible</h1>
+            </div>
+         </div>
+         <div className={classes.form}>
+         <form className={classes.form} onSubmit={submitHandler}>
+         <h3>Search</h3>
+         <input type="text" required id="image" ref={imageInputRef} onSubmit={onSearchMovie} />
+         </form>
+         </div>
+     </section>
+    )
+    
+}
+
+export default HeroPageSection;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+
+import MoviesFormSection from "../movies/MoviesForm";
+
+function SearchBarSection() {
+    
+
+    return (<section>
+        <h2>Search</h2>
+        <MoviesFormSection  />
+    </section>
+    )
+}
+
+export default SearchBarSection;
